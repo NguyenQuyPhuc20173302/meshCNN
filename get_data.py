3630632935b6e4f7d068f469e59cc90c1d12ff0a
Nguyễn Quý Phúc <nguyenquyphuc1591999@gmail.com>
2021-07-11 13:57:04 +0700

    OK

--- get_data.py
+++ get_data.py
@@ -1,9 +1,11 @@
 import os
 import zipfile
+import  shutil
 
-if not os.path.exists('datasets'):
-    os.mkdir('datasets')
-    fantasy_zip = zipfile.ZipFile('shrec_16.zip')
-    fantasy_zip.extractall('datasets')
-    fantasy_zip.close()
+shutil.rmtree('datasets')
+# if not os.path.exists('datasets'):
+#     os.mkdir('datasets')
+#     fantasy_zip = zipfile.ZipFile('shrec_16.zip')
+#     fantasy_zip.extractall('datasets')
+#     fantasy_zip.close()
 
