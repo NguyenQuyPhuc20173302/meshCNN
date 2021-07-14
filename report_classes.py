9fefe90180c7b430c75df2d783b1be5e44c3dc4b
Nguyễn Quý Phúc <nguyenquyphuc1591999@gmail.com>
2021-07-14 10:22:05 +0700

    'ok'

--- report_classes.py
+++ report_classes.py
@@ -2,6 +2,7 @@ from options.test_options import TestOptions
 from data import DataLoader
 from models import create_model
 from util.writer import Writer
+from sklearn.metrics import classification_report
 
 
 def run_test(epoch=-1):
@@ -9,14 +10,16 @@ def run_test(epoch=-1):
     opt = TestOptions().parse()
     opt.serial_batches = True  # no shuffle
     dataset = DataLoader(opt)
+    name_label = dataset.dataset.classes
     model = create_model(opt)
     writer = Writer(opt)
     # test
     writer.reset_counter()
     for i, data in enumerate(dataset):
         model.set_input(data)
-        ncorrect, nexamples = model.test()
+        ncorrect, nexamples, pre_, label_origin = model.test()
         writer.update_counter(ncorrect, nexamples)
+        print(classification_report(label_origin, pre_, target_names=name_label))
     writer.print_acc(epoch, writer.acc)
     return writer.acc
 
